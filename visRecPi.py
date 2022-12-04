de870dc506d1964ab065da68e987e5bac557230f
gabbyca <gabrielle_cammarata@mynbps.org>
2022-12-04 12:26:54 -1000

    open video works

--- visRecPi.py
+++ visRecPi.py
@@ -1,37 +1,25 @@
 import cv2
-import matplotlib
-
-stream = cv2.VideoCapture(0)
-grab, frame = stream.read()
-matplotlib.pyplot.imshow(frame)
-
-grab, frame = stream.read()
-matplotlib.pyplot.imshow(frame)
+import time
 
 
-
-
-
-
-#-------------------------------------------------
-#acess video, webcam 
-#stream = cv2.VideoCapture(0)
+camera = 'tcp://0.0.0.0:5000'
+stream = cv2.VideoCapture(camera)
 
 #if stream is not opened print no stream 
-#if not stream.isOpened(): 
-    #print("no stream")
-    #exit()
+if not stream.isOpened(): 
+    print("no stream")
+    exit()
 
 #if stream found, return the frames of the screen 
-#while(True):
-    #ret, frame = stream.read()
-    #cv2.imshow("Webcam", frame)
-    #if cv2.waitKey(1) == ord('q'):
-        #break
-    #if not ret:
-        #print("stream ended")
-        #break
+while(True):
+    ret, frame = stream.read()
+    if not ret:
+        print("stream ended")
+        break
+    cv2.imshow("Webcam", frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
     
 
-#stream.release()
-#cv2.destroyAllWindows()
+stream.release()
+cv2.destroyAllWindows()
