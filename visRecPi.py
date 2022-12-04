7c1285f862b1a9001b6409f3fab9103671ae96da
gabbyca <gabrielle_cammarata@mynbps.org>
2022-12-04 11:31:39 -1000

    capture video

--- visRecPi.py
+++ visRecPi.py
@@ -1,3 +1,37 @@
 import cv2
+import matplotlib
 
-#test 
+stream = cv2.VideoCapture(0)
+grab, frame = stream.read()
+matplotlib.pyplot.imshow(frame)
+
+grab, frame = stream.read()
+matplotlib.pyplot.imshow(frame)
+
+
+
+
+
+
+#-------------------------------------------------
+#acess video, webcam 
+#stream = cv2.VideoCapture(0)
+
+#if stream is not opened print no stream 
+#if not stream.isOpened(): 
+    #print("no stream")
+    #exit()
+
+#if stream found, return the frames of the screen 
+#while(True):
+    #ret, frame = stream.read()
+    #cv2.imshow("Webcam", frame)
+    #if cv2.waitKey(1) == ord('q'):
+        #break
+    #if not ret:
+        #print("stream ended")
+        #break
+    
+
+#stream.release()
+#cv2.destroyAllWindows()
