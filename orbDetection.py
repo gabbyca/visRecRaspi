f9e4a9966b7b8d3987706dd4f144c4683ba20a91
gabbyca <98795656+gabbyca@users.noreply.github.com>
2022-12-10 16:23:43 -0500

    only drillpress and bandsaw with feature detection

--- orbDetection.py
+++ orbDetection.py
@@ -1,13 +1,12 @@
 import cv2
+import numpy as np
 
 bandsaw =cv2.imread('bandsaw.jpg', 0)
-mill=cv2.imread('bigDrillPress.jpg', 0)
 drillPress= cv2.imread('drillPress.jpg', 0)
 
 #query images 
 sanderBandsawChopsaw = cv2.imread('sanderBandsawChopsaw.jpg', 0)
 sanderDrillBandsaw = cv2.imread('sanderDrillBandsaw.jpg', 0)
-millChopsaw = cv2.imread('millChopsaw.jpg', 0)
 
 #orb detection 
 orb = cv2.ORB_create()
@@ -16,9 +15,6 @@ orb = cv2.ORB_create()
 kp1 , des1 = orb.detectAndCompute(bandsaw, None)
 kp2 , des2 = orb.detectAndCompute(sanderBandsawChopsaw, None)
 kp3, des3 = orb.detectAndCompute(sanderDrillBandsaw, None)
-#mill
-kp4 , des4 = orb.detectAndCompute(mill, None)
-kp5, des5 = orb.detectAndCompute( millChopsaw, None)
 #drillPress
 kp6 , des6 = orb.detectAndCompute(drillPress, None)
 kp7, des7 = orb.detectAndCompute(sanderDrillBandsaw, None)
@@ -28,8 +24,6 @@ bf = cv2.BFMatcher()
 
 mb1 = bf.knnMatch(des1, des2, k=2)
 mb2 = bf.knnMatch(des1, des3, k=2)
-
-mm1 = bf.knnMatch(des4, des5, k=2)
 
 md1 = bf.knnMatch(des6, des7, k=2)
 
@@ -42,11 +36,6 @@ for m,n in mb1:
 for m,n in mb2:
     if m.distance < 0.75*n.distance:
         good.append([m])
-
-for m,n in mm1:
-    if m.distance < 0.75*n.distance:
-        good.append([m])
-
 for m,n in md1:
     if m.distance < 0.75*n.distance:
         good.append([m])
@@ -54,5 +43,4 @@ for m,n in md1:
 
 img5 = cv2.drawMatchesKnn(bandsaw, kp1, sanderBandsawChopsaw, kp2, good, None, flags=2)
 img6 = cv2.drawMatchesKnn(bandsaw, kp1, sanderDrillBandsaw, kp3, good, None, flags=2)
-img7 = cv2.drawMatchesKnn(mill, kp4, millChopsaw, kp5, good, None, flags=2)
 img8 = cv2.drawMatchesKnn(drillPress, kp6, sanderDrillBandsaw, kp7, good, None, flags=2)
