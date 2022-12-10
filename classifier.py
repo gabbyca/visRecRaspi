a2feea963fa6f6b063381db49b503101555a0801
gabbyca <gabrielle_cammarata@mynbps.org>
2022-12-10 17:55:22 -0500

    ORB detect final not tested

---
+++ classifier.py
@@ -0,0 +1,48 @@
+import cv2
+import numpy as np
+import os
+
+orb=cv2.ORB_create()
+path = 'imagesSingle'
+
+images = []
+classNames =[]
+
+list = os.listdir(path)
+
+for cl in list:
+    imgCur = cv2.imread(f'[{path}/{cl}', 0)
+    images.append(imgCur)
+    classNames.append(os.path.splitext(cl)[0])
+
+def findDes(images):
+    desList =[]
+    for img in images:
+        kp,des=orb.detectAndCompute(img, None)
+        desList.append(des)
+    return desList
+
+def findID(img, desList):
+    kp2,des2=orb.detectAndCompute(img, None)
+    #brute force matcher
+    bf = cv2.BFMatcher()
+    matchList=[]
+    finVal = -1
+    try:
+        for des in desList:
+            matches =bf.knnMatch(des, des2, k=2)
+            good =[]
+            for m,n in matches:
+                if m.distance < 0.75*n.distance:
+                    good.append([m])
+            matchList.append(len(good))
+    except:
+        pass
+    #print(matchList)
+    if len(matchList) != 0:
+        if max(matchList) >= 1:
+            finVal =matchList.index(max(matchList))
+    return finVal
+
+desList=findDes(images)
+
--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 import cv2
 import time
 import numpy as np
-import orbDetection
+import classifier
 
 camera = 'tcp://0.0.0.0:5000'
 stream = cv2.VideoCapture(camera)
 
-
-img5 = orbDetection.img5
-img6 = orbDetection.img6
-img8 = orbDetection.img8
-
-cv2.imshow('img5', img5)
-cv2.imshow('img6', img6)
-cv2.imshow('img8', img8)
 
 fps = int(stream.get(cv2.CAP_PROP_FPS))
 width = int(stream.get(3))
@@ -26,16 +18,18 @@ current_time = time.strftime("%H:%M:%S", t)
 #write video (save)
 output = cv2.VideoWriter('videoStorageOpencv/' + current_time + '.avi',cv2.VideoWriter_fourcc('M','J','P','G'),fps,size)
 
+desList = classifier.desList
+
 #if stream found, return the frames of the screen 
 while(True):
-    ret, frame = stream.read()
-    if not ret:
-        print("stream ended")
-        break
-    
-    cv2.resize(frame, (width,height))    
+    ret, frame = stream.read()  
     output.write(frame)
-    cv2.imshow("Webcam", frame)
+    imgOriginal = frame.copy()
+  
+    id = classifier.findID(frame, desList)
+    if id != -1:
+        cv2.putText(imgOriginal, classifier.classNames[id], (50,50), cv2.FONT_KERSHEY_COMPLEX,1,(0,0,255),1)
+
     if cv2.waitKey(1) == ord('q'):
         break
     
@@ -44,4 +38,3 @@ while(True):
 output.release()
 stream.release()
 cv2.destroyAllWindows()
-print("video saved")
