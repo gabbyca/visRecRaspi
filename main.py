c7cd2764c25fb3d43c06ee1f409b0fba44ec2942
gabbyca <gabrielle_cammarata@mynbps.org>
2022-12-11 18:16:01 -0500

    face rec works

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ camera = 'tcp://0.0.0.0:5000'
 stream = cv2.VideoCapture(camera)
 
 faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+def findFace(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(gray, 1.3, 3)
+    for(x,y,w,h) in faces:
+        frame = cv2.rectangle(frame, (x,y), (x+w, y+h), color =(0,255,0), thickness=5)
+    return frame
+
 #set dimensions
 fps = int(stream.get(cv2.CAP_PROP_FPS))
 width = int(stream.get(3))
@@ -29,20 +36,18 @@ while(True):
     #read frames of stream
     ret, frame = stream.read()  
     imgOriginal = frame.copy()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    cv2.imshow('w', frame)
+   
     #classifier (drillpress & bandsaw detection)
-    id = classifier.findID(frame, desList)
-    if id != -1:
-        cv2.putText(imgOriginal, classifier.classNames[id], (50,50), cv2.FONT_KERSHEY_COMPLEX,1,(0,0,255),1)
+    # id = classifier.findID(frame, desList)
+    # if id != -1:
+    #     cv2.putText(imgOriginal, classifier.classNames[id], (50,50), cv2.FONT_KERSHEY_COMPLEX,1,(0,0,255),1)
 
     #facialRec
-    faces = faceCascade.detectMultiScale(gray, 1.1, 4)
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    frame = findFace(frame)
 
+    cv2.imshow('w', frame)
     output.write(frame)
+
     if cv2.waitKey(1) == ord('q'):
         break
     
