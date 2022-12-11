22d5b072cf2bb526643669ff174b8173b9e0c142
gabbyca <gabrielle_cammarata@mynbps.org>
2022-12-10 23:23:31 -0500

    failed w vid

--- facialRec.py
+++ facialRec.py
@@ -1,11 +1,10 @@
 import cv2
-import main
 
 
-def findFaces():
-    faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-    gray = cv2.cvtColor(main.frame, cv2.COLOR_BGR2GRAY)
-    faces = faceCascade.detectMultiScale(gray, 1.1, 4)
-    for (x, y, w, h) in faces:
-        cv2.rectangle(main.frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+#def findFaces():
+    # faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+    # faces = faceCascade.detectMultiScale(gray, 1.1, 4)
+    # for (x, y, w, h) in faces:
+    #     cv2.rectangle(main.frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import facialRec
 camera = 'tcp://0.0.0.0:5000'
 stream = cv2.VideoCapture(camera)
 
+faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 #set dimensions
 fps = int(stream.get(cv2.CAP_PROP_FPS))
 width = int(stream.get(3))
@@ -27,18 +28,21 @@ desList = classifier.desList
 while(True):
     #read frames of stream
     ret, frame = stream.read()  
-    output.write(frame)
     imgOriginal = frame.copy()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    cv2.imshow('w', frame)
     #classifier (drillpress & bandsaw detection)
     id = classifier.findID(frame, desList)
     if id != -1:
         cv2.putText(imgOriginal, classifier.classNames[id], (50,50), cv2.FONT_KERSHEY_COMPLEX,1,(0,0,255),1)
 
     #facialRec
-    facialRec.findFaces()
-   
+    faces = faceCascade.detectMultiScale(gray, 1.1, 4)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
+    output.write(frame)
     if cv2.waitKey(1) == ord('q'):
         break
     
